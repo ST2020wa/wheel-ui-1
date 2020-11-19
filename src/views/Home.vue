54bb685eb54ee9893da1bcc0db8f460e013eb149
ST Li <kidtime@126.com>
2020-11-19 18:21:35 +0800

    update Home css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <h2>An AWESOME UI-frame</h2>
       <p class="actions">
         <a>GitHub</a>
-        <a>Get Start</a>
+        <a>Get Started</a>
       </p>
     </div>
   </div>
@@ -17,3 +17,26 @@
     components: {Topnav}
   }
 </script>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
