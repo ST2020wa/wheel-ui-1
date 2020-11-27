1f0d63701eeaebf280fb0ac8a9a6fcce1b08101e
ST Li <kidtime@126.com>
2020-11-27 17:28:44 +0800

    initiate button component

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,4 +2,12 @@
   <div>
     Hey I'm ButtonDemo!
   </div>
+  <h3>Example 1</h3>
+  <div><Button>Hello</Button></div>
 </template>
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+  components: {Button}
+}
+</script>
