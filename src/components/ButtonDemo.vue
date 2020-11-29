11079d08db8afe0d3b2f507d0f8b6f9d08ba0e19
ST Li <kidtime@126.com>
2020-11-29 17:40:41 +0800

    create button component-v2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,23 +2,18 @@
   <div>
     Hey I'm ButtonDemo!
   </div>
-  <h3>Example 1</h3>
+  <h1>
+    Example 1
+  </h1>
   <div>
-    <Button>Hello</Button>
-    <Button theme="button">Hello</Button>
-    <Button theme="link">Hello</Button>
-    <Button theme="text">Hello</Button>
+    <Button>
+      Halo!
+    </Button>
   </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components: {Button},
-  setup(){
-    const onClick = ()=>{
-      console.log('hi')
-    }
-    return {onClick}
-  }
+  components: { Button },
 }
 </script>
